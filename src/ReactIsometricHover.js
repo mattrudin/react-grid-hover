5a0109b6cf8244f7f0bb5482535b825a009c9cf5
Matthias Rudin <matthias.rudin@live.com>
2019-01-06 16:05:58 +0100

    Added css transformation and dependencies

--- src/ReactIsometricHover.js
+++ src/ReactIsometricHover.js
@@ -3,24 +3,46 @@ import styled from 'styled-components'
 import { gridDefiner } from './utilities'
 
 const ReactIsometricHover = (props) => { 
-    const columnsNumber = gridDefiner(props.children)
-    const rowsNumber = gridDefiner(props.children) + 1
+    const { children, itemSize, skew='left' } = props
+    const columnsNumber = gridDefiner(children)
+    const rowsNumber = gridDefiner(children) + 1
 
     const GridContainer = styled.div`
         display: grid;
-        grid-gap: 10px;
+        grid-gap: 20px;
+        width: ${itemSize * columnsNumber}px;
+        height: ${itemSize * rowsNumber}px;
         grid-template-columns: repeat(${columnsNumber}, 1fr);
         grid-template-rows: repeat(${rowsNumber}, 1fr);
+        transform: skewX(${skew === "right" ? "-" : ""}45deg);
+    `
+
+    const GridChild = styled.div`
+        width: ${itemSize}px;
+        height: ${itemSize}px;
+        box-shadow: -20px 30px 5px grey;
+        transition-duration: 0.5s;
+        :hover {
+            transform: translate3d(0px,-10px,10px);
+        }
+        img {
+            width: 100%;
+            height: 100%;
+        }
     `
     
     return ( 
         <>
             <GridContainer>
-                {props.children}
+                {children.map(child => (
+                    <GridChild>
+                        {child}
+                    </GridChild>
+                ))}
             </GridContainer>
         </>
      );
 }
 
 
-export default ReactIsometricHover;props
+export default ReactIsometricHover
